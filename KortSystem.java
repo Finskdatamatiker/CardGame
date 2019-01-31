014a313ecdd194cbcb0bb0db39f8eca8e3eec86b
Päivi Eversbusch <42863950+Finskdatamatiker@users.noreply.github.com>
2019-01-31 19:59:16 +0100

    Update KortSystem.java

--- KortSystem.java
+++ KortSystem.java
@@ -1,5 +1,5 @@
 /**
-@author Päivi Eversbusch
+@author PÃ¤ivi Eversbusch
 @version 1.0
 @since 29.1.2019
 */
@@ -9,7 +9,7 @@ public class KortSystem
 {
  /**
  * Denne klasse laver en instans af SaetGenerator og har ansvar for at 
- * udføre handlinger, som vises i en menu
+ * udfÃ¸re handlinger, som vises i en menu
  */
       private Scanner console;
       SaetGenerator saetGenerator = new SaetGenerator(); 
@@ -17,7 +17,7 @@ public class KortSystem
       public KortSystem()
       {
       /**
-      * I konstruktøren laver vi en instans af Scanner og kalder på 
+      * I konstruktÃ¸ren laver vi en instans af Scanner og kalder pÃ¥ 
       * metoden, som koerer progammet
       */
           console = new Scanner(System.in);
@@ -31,27 +31,29 @@ public class KortSystem
          while(koerProgram)
          {
          /**
-         * Valget laeses med charAt(), fordi der er ikke flere end
-         * fire valgmuligheder. Var der felre end 9, skulle man justere
-         * metoden, fordi charAt(0) læser kun ét tegn. 
-         * String dummy laeser enter, men trykker på efter indtastningen.
+         * Valget laeses med int
+         * String dummy laeser enter, men trykker pÃ¥ efter indtastningen.
          */
             System.out.println(visMenu());
-            char valg = console.next().charAt(0);
-            String dummy = console.nextLine(); 
+            while(!console.hasNextInt())
+            {
+                 String dummie = console.nextLine();
+                 System.out.println("Skriv et heltal.");
+            }
+            int valg = console.nextInt();
             
-            if (valg == '0')
+            if (valg == 0)
             {
                 System.out.println("Afslutter programmet");
                 koerProgram = false;
             } 
               
-            else if (valg == '1')
+            else if (valg == 1)
             {
             /**
-            * Først henter vi et sæt kort og derefter blander vi dem.
+            * FÃ¸rst henter vi et sÃ¦t kort og derefter blander vi dem.
             */
-               System.out.println("\nSæt før vi blander\n");
+               System.out.println("\nSÃ¦t fÃ¸r vi blander\n");
                for(int i = 0; i < 52; i++)
                {
                   System.out.println(saetGenerator.getSaet().getKort(i));
@@ -59,7 +61,7 @@ public class KortSystem
                
                saetGenerator.blandKort(saetGenerator.getSaet());
                
-               System.out.println("\nSæt efter at vi har blandet\n");
+               System.out.println("\nSÃ¦t efter at vi har blandet\n");
                for(int i = 0; i < saetGenerator.getSaet().getAntal(); i++)
                {
                   System.out.println(saetGenerator.getSaet().getKort(i));
@@ -68,44 +70,43 @@ public class KortSystem
                
              }
       
-             else if (valg == '2')
+             else if (valg == 0)
              {
              /**
-             * Først trækker vi det øverste kort, så blander vi og  
-             * trækker et nyt kort.
+             * FÃ¸rst trÃ¦kker vi det Ã¸verste kort, sÃ¥ blander vi og  
+             * trÃ¦kker et nyt kort.
              */
-                System.out.println("det øverste kort før:   " + saetGenerator.getSaet().getKort(0));
+                System.out.println("det Ã¸verste kort fÃ¸r:   " + saetGenerator.getSaet().getKort(0));
                 saetGenerator.blandKort(saetGenerator.getSaet());
-                System.out.println("det øverste kort efter: " + saetGenerator.getSaet().getKort(0));
+                System.out.println("det Ã¸verste kort efter: " + saetGenerator.getSaet().getKort(0));
              }   
-             else if (valg == '3')
+             else if (valg == 3)
              {
              /**
-             * Brugeren kan bestemme, hvor mange kort vi trækker fra 
-             * sættet og så fortæller vi, hvor mange der er tilbage. 
-             * Kortenes værdier har ikke nogen betydning, så vi 
+             * Brugeren kan bestemme, hvor mange kort vi trÃ¦kker fra 
+             * sÃ¦ttet og sÃ¥ fortÃ¦ller vi, hvor mange der er tilbage. 
+             * Kortenes vÃ¦rdier har ikke nogen betydning, sÃ¥ vi 
              * blander dem ikke.
              * Hvis man indtaster et bogstav eller et tal, som ikke
-             * er mellem 0 og 52, bliver man bedt om at vælge et tal
+             * er mellem 0 og 52, bliver man bedt om at vÃ¦lge et tal
              * mellem 0 og 52.
              */
-               System.out.println("Hvor mange kort vil du trække?");
+               System.out.println("Hvor mange kort vil du trÃ¦kke?");
                
                while(!console.hasNextInt())
                {
                console.nextLine();
-               System.out.println("Skriv et tal.");
+               System.out.println("Skriv et heltal.");
                }
                
                boolean runWhile = true;
                while (runWhile)
                {
                   int antal = console.nextInt();
-                  String enter = console.nextLine(); 
                 
                   if(antal < 0 || antal > 52)
                   {
-                     System.out.println("Vælg et tal mellem 0 og 52");
+                     System.out.println("VÃ¦lg et tal mellem 0 og 52");
                       while(!console.hasNextInt())
                      {
                         console.nextLine();
@@ -130,7 +131,7 @@ public class KortSystem
                } 
                else
                {
-                System.out.println("Vælg et af menupunkterne.\n");
+                System.out.println("VÃ¦lg et af menupunkterne.\n");
               }
             }        
          }
@@ -139,10 +140,10 @@ public class KortSystem
          {
             String menu = "Velkommen!\n";
             menu += "Hvad vil du foretage dig?\n";
-            menu += "1: Bland et sæt kort.\n";
-            menu += "2: Bland et sæt kort og træk det øverste kort.\n";
-            menu += "3: Træk et antal kort og se, hvor mange der er tilbage.\n";
-            menu += "0: Afslut programmet og du får et nyt sæt kort.\n";
+            menu += "1: Bland et sÃ¦t kort.\n";
+            menu += "2: Bland et sÃ¦t kort og trÃ¦k det Ã¸verste kort.\n";
+            menu += "3: TrÃ¦k et antal kort og se, hvor mange der er tilbage.\n";
+            menu += "0: Afslut programmet og du fÃ¥r et nyt sÃ¦t kort.\n";
             return menu;                     
          }
    }
